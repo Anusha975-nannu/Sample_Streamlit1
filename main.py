8ef0e61580e7f3b37193b65bce76a1ff08b67b9f
Anusha975-nannu <pillianusha975@gmail.com>
2025-09-25 18:40:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
 st.title("Hello, Anusha")
-st.subheader("This is my own streanlit app")
+st.subheader("This is my own streamlit app")
 st.write("Streamlit is an easy platform to create attractive websites applications with the python programming language")
