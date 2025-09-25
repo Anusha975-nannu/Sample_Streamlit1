ea47a0ad5883975aa5f40680913d9633bbf0d7bc
Anusha975-nannu <pillianusha975@gmail.com>
2025-09-25 18:36:59 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title("Hello, Anusha")
+st.subheader("This is my own streanlit app")
+st.write("Streamlit is an easy platform to create attractive websites applications with the python programming language")
